ec4a6839c9ff4c6873061cca274766e60e5ed4ee
LockJay <LockJay@users.noreply.github.com>
2015-05-26 11:31:58 +1000

    added a lot of hacks dude

---
+++ SoManyHacks/SoManyHacks/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	int daHax = 0;
+	while (true)
+	{
+		daHax += 1;
+		std::cout << daHax;
+		//this next bit makes it so that only one number prints at a time
+		//system("cls");
+	}
+}
